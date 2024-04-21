1202974876f4acadf05604040202cb1265ad517e
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2024-04-21 04:30:23 -0400

    remove (most) unused functions after change to LECS

--- fighter/js/script.js
+++ fighter/js/script.js
@@ -2,50 +2,16 @@
 
 import { Game } from "./lecs/game.js";
 
-//Objeto base para los personajes
-class Character {
-    constructor(name, health, damage) {
-      //Atributos
-      this.name = name;
-      this.health = health;
-      this.maxhealth = health;
-      this.damage = damage;
-    }
-    //Verifica si el personaje esta vivo
-    isAlive() {
-      return this.health > 0;
-    }
-  
-    //Ataca a otro personaje seleccionado
-    attack(target) {
-      console.log(`${this.name} deals ${this.damage} DMG to ${target.name}`);
-      target.health -= getRndInteger(5,10);
-    }
-  
-    //Retorna la información actual del personaje
-    status() {
-      return `${this.name} - HP ${this.health}/${this.maxhealth}`;
-    }
-}
-  
-  //Función para combatir
-
-
-
 
   
   //Creación de personajes
-  const hero = new Character("Heroe", getRndInteger(1,100), 110);
-  const enemy = new Character("Limo", getRndInteger(1,100), 40);
   let heroBar=document.getElementById("heroHealth");
   let enemyBar=document.getElementById("enemyHealth");
 
   let pressedKeys=[]
   const speed=5
 
-  //repres
-  let heroRepr=document.getElementById("heroRepr")
-  let enemyRepr=document.getElementById("enemyRepr")
+  
     
   function setup(){
     
@@ -110,178 +76,24 @@ class Character {
 
   }
 
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
-}
-
-  function sleep(millis)
-{
+function sleep(millis){
     var date = new Date();
     var curDate = null;
     do { curDate = new Date(); }
     while(curDate-date < millis);
 }
 
-function attack(attacker,attacked){
-    if (!attacker.isAlive()){
-        console.log("attacker is dead")
-        return;
-    }
-    if (!attacked.isAlive()){
-        console.log("attacked is dead!")
-        return;
-    }
-    attacker.attack(attacked)
-    calculateHealthBar()
-}
-
-
-function move(direction,repr){
-  if (willBeOOB(repr,direction)){
-    return;
-  }
-  if (direction=="u"){
-    repr.style.top=(parseFloat(repr.style.top) || 0) - speed + 'px';
-  }
-  else if(direction=="r"){
-    repr.style.left=(parseFloat(repr.style.left) || 0) + speed + 'px';
-  }
-  else if (direction=="d"){
-    repr.style.top=(parseFloat(repr.style.top) || 0) + speed + 'px';
-  }
-  else if (direction=="l"){
-    repr.style.left=(parseFloat(repr.style.left) || 0) - speed + 'px';
-  }
-}
 
 
 
-function willBeOOB(repr,direction){
-let arena=document.getElementById("arenaZone");
-switch (direction) {
-  case "u":
-    if (repr.getBoundingClientRect().top<arena.getBoundingClientRect().top){
-      console.log("pos: ",heroRepr.getBoundingClientRect().top,arena.getBoundingClientRect().top)
-      return true
-    }
-    break;
-  case "d":
-    if (repr.getBoundingClientRect().bottom+10>arena.getBoundingClientRect().bottom){
-      console.log("pos: ",heroRepr.getBoundingClientRect().bottom,arena.getBoundingClientRect().bottom)
-      return true
-    }
-    break;
-  case "l":
-    if (repr.getBoundingClientRect().left-9<arena.getBoundingClientRect().left){
-      console.log("pos: ",heroRepr.getBoundingClientRect().left,arena.getBoundingClientRect().left)
-      return true
-    }
-    break;
-    case "r":
-      if (repr.getBoundingClientRect().right+10>arena.getBoundingClientRect().right){
-        console.log("pos: ",heroRepr.getBoundingClientRect().left,arena.getBoundingClientRect().left)
-        return true
-      }
-      break;
-
-  default:
-    break;
-}
-}
 
 
-
-setup()
-fillBars();
-calculateHealthBar();
-
-//window.onload=setupDocumentPositions();
-
-alert("Vida héroe (z): "+hero.maxhealth+"\n Vida enemigo (n): "+enemy.maxhealth)
 let game=new Game();
 game.start();
-
-  
-
-/*window.onkeydown=function(key){
-  if (pressedKeys.indexOf(key.key)!=-1){
-    return
-  }
-    console.log("keydown ",key)
-    if (key.key=='z'){
-        attack(hero,enemy)
-        
-    }
-    else if (key.key=='n'){
-        attack(enemy,hero)
-    }
-    else if (key.key=='ArrowDown'){
-      pressedKeys.push(key.key)
-    }
-    else if (key.key=='ArrowUp'){
-      pressedKeys.push(key.key)
-    }
-    else if (key.key=='ArrowLeft'){
-      pressedKeys.push(key.key)
-    }
-    else if (key.key=='ArrowRight'){
-      pressedKeys.push(key.key)
-    }
-    else if (key.key=="w"){
-      pressedKeys.push(key.key)
-    }
-    else if (key.key=="s"){
-      pressedKeys.push(key.key)
-    }
-    else if (key.key=="a"){
-      pressedKeys.push(key.key)
-    }
-    else if (key.key=="d"){
-      pressedKeys.push(key.key)
-    }
-    console.log("pressed keys ",pressedKeys)
-}
-
-function executeMovement(){
-  
-    pressedKeys.forEach(key=> {
-      if (key=='ArrowDown'){
-        move("d",heroRepr)
-      }
-      else if (key=='ArrowUp'){
-        move("u",heroRepr)
-      }
-      else if (key=='ArrowLeft'){
-        move("l",heroRepr)
-      }
-      else if (key=='ArrowRight'){
-        move("r",heroRepr)
-      }
-      else if (key=="w"){
-        move("u",enemyRepr)
-      }
-      else if (key=="s"){
-        move("d",enemyRepr)
-      }
-      else if (key=="a"){
-        move("l",enemyRepr)
-      }
-      else if (key=="d"){
-        move("r",enemyRepr)
-      }
-      
-    });
-    
-  
-}
-
-window.setInterval(executeMovement,20)
-
-window.onkeyup=function(key){
-  let index=pressedKeys.indexOf(key.key)
-  if (index!=-1){
-    console.log("keyup ",key.key)
-    pressedKeys.splice(index,1)
-  }
-}*/
-
+let resetButton=document.getElementById("resetButton")
+resetButton.onclick=function (btn){
+                                  clearTimeout(game.gameUpdateId)
+                                  game=new Game();
+                                  game.start();
+                                  resetButton.blur()
+                              }
