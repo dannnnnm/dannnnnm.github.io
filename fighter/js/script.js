08b01750d7941b2a0e0a480b1af2d0ed0051b21c
Manuel <72231982+zendar426@users.noreply.github.com>
2024-04-26 09:20:39 -0400

    add reloaded music from "pokemon league"

--- fighter/js/script.js
+++ fighter/js/script.js
@@ -114,7 +114,11 @@ selectEdmMusic.onclick=function(){
   changeMusic("audio/princess.m4a")
   selectEdmMusic.blur()
 }
-
+let selectRelMusic=document.getElementById("reloadedMusic")
+selectRelMusic.onclick=function(){
+  changeMusic("../audio/snd_battle_accion.mid")
+  selectRelMusic.blur()
+}
 
 audio.play().then(
   (result)=>{
